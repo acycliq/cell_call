7430cae7d296f5cb71d6c6c2c586390b37f4b36c
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-11-06 16:35:04 +0000

    first commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+class algo:
+    def __init__(self):
+        print('in algo')
+
+    def callCells(self):
+        '''
+        Assign cells to classes
+        '''
+        print('in algo:callCells')
+
+    def callSpots(self):
+        '''
+        Assign spots to the neighbouring cells
+        '''
+
+
