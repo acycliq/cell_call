e0527697cb8caff35fc043be4a81ed718ddc9d87
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-12-14 15:16:59 +0000

    Configuration file

---
+++ src/config.py
@@ -0,0 +1,27 @@
+
+default = {'issPath': '../data/default/iss.mat',
+           'cellMap': '../data/default/CellMap.mat',
+           'geneset': '../data/GeneSet.mat',
+           'name': 'default'}
+
+threeOneLeft = {'issPath': '../data/3-1_left/iss.mat',
+                'cellMap': '../data/3-1_left/CellMap.mat',
+                'geneset': '../data/GeneSet.mat',
+                'name': '161220KI_3_1_left'}
+
+threeOneRight = {'issPath': '../data/3-1_right/iss.mat',
+                'cellMap': '../data/3-1_right/CellMap.mat',
+                'geneset': '../data/GeneSet.mat',
+                'name': '161220KI_3_1_right'}
+
+fourThreeRight = {'issPath': '../data/4-3_right/iss.mat',
+                  'cellMap': '../data/4-3_right/CellMap.mat',
+                  'geneset': '../data/GeneSet.mat',
+                  'name': '161220KI_4_3_right'}
+
+
+settings = {'default': default,
+            '3_1_left': threeOneLeft,
+            '3_1_right': threeOneRight,
+            '4_3_right': fourThreeRight,
+            }
