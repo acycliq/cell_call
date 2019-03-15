f230d012703653175b727f37759885ebb6b94f26
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2019-03-15 09:03:56 +0000

    Added one more checkbox in the heatmap tab. looks ok, double check the heatmap checkboxes feed in the correct data

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -15,6 +15,10 @@ function renderHeatmapTab(selected) {
     if (document.getElementById('genes42').checked){
         checkBox0 = '42genes'
     }
+    else if (document.getElementById('genes62').checked)
+    {
+        checkBox0 = '62genes'
+    }
     else {
         checkBox0 = '98genes'
     }
@@ -57,4 +61,62 @@ function renderHeatmapTab(selected) {
         console.log('json parsed!!');
         renderHeatmap(dataset);
     });
+}
+
+
+var sectionChartFilters = document.getElementById('section-chart-controls');
+var checkItAll = sectionChartFilters.querySelector('input[name="cb:select-all"]');
+var inputs = sectionChartFilters.querySelectorAll('tbody>tr>td>input');
+
+
+inputs.forEach(function (input) {
+    input.addEventListener('change', function () {
+        if (!this.checked) {
+            checkItAll.checked = false;
+        } else if (!checkItAll.checked) {
+            var allChecked = true;
+            for (var i = 1; i < inputs.length; i++) {
+                if (!inputs[i].checked) {
+                    allChecked = false;
+                }
+            }
+
+            if (allChecked) {
+                checkItAll.checked = true;
+            }
+        }
+
+        var selected = getSelected(inputs);
+        var filteredSectionData = cellData.filter(function (el) {
+            return selected.includes(el.IdentifiedType)
+        })
+        sectionChart(filteredSectionData)
+
+    });
+
+});
+
+checkItAll.addEventListener('change', function () {
+    inputs.forEach(function (input) {
+        input.checked = checkItAll.checked;
+    });
+
+    var selected = getSelected(inputs);
+});
+
+function getSelected(inputs) {
+    //Loop over all selected
+    var selected = [];
+    for (var i = 1; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            if (inputs[i].name === 'Cck') {
+                selected.push('Cck Calb1/Slc17a8*', 'Cck Cxcl14-', 'Cck Cxcl14+', 'Cck Vip Cxcl14-', 'Cck Vip Cxcl14+');
+            } else if (inputs[i] === 'PC') {
+                selected.push('PC', 'PC Other1', 'PC Other2')
+            } else {
+                selected.push(inputs[i].name);
+            }
+        }
+    }
+    return selected
 }
