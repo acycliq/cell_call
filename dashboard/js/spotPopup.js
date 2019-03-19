b573f4da0ffb896041445ae49b48ca323f9f1011
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2019-03-19 09:14:21 +0000

    looks ok

--- dashboard/js/spotPopup.js
+++ dashboard/js/spotPopup.js
@@ -1,70 +1,76 @@
-function spotPopup(dataset) {
+function spotPopup(datapoint) {
+// From https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4
+//     and http://bl.ocks.org/andrew-reid/11602fac1ea66c2a6d7f78067b2deddb
+
+    var data = [];
+    for (var i = 0; i < datapoint.labels.length; i++) {
+        data.push({
+            Prob: +datapoint.Prob[i],
+            labels: datapoint.labels[i],
+        })
+    }
 
 
-    var width = 400;
-    var height = 150;
-    var margin = {left: 40, right: 15, top: 40, bottom: 40};
+// set the dimensions and margins of the graph
+    var width = 200;
+    var height = 80;
+    var margin = {left:40,right:15,top:20,bottom:40};
 
-    var svg = d3.select("#bar-chart").select("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-    var g = svg.append("g")
-        .attr("transform", "translate(" + [margin.left, margin.top] + ")");
-
-    var y = d3.scaleLinear()
-        .domain([0, 1])
-        .range([height, 0]);
-
-    var yAxis = d3.axisLeft()
-        .ticks(4)
-        .scale(y);
-    g.append("g").call(yAxis);
-
-    // var x = d3.scaleBand()
-    //     .domain(d3.range(4))
-    //     .range([0, width]);
-
-    var x = d3.scaleBand()
-        .domain(dataset.labels)
-        .range([0, width])
-        .padding(0.1);
+// set the ranges
+    var x = d3.scaleBand().range([0, width]).padding(0.1);
 
     var xAxis = d3.axisBottom()
-        .scale(x);
+                .scale(x);
 
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .attr("text-anchor", "middle")
-        // .attr("transform", "rotate(-90)translate(-12,-15)")
+    var y = d3.scaleLinear().range([height, 0]);
 
-    var rects = g.selectAll("rect")
-        .data(dataset.Prob)
+    var yAxis = d3.axisLeft()
+                .ticks(4)
+                .scale(y);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    var svg = d3.select("body").select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    x.domain(data.map(function (d) {
+        return d.labels;
+    }));
+    y.domain([0, d3.max(data, function (d) {
+        return d.Prob;
+    })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
         .enter()
         .append("rect")
-        .attr("y", height)
-        .attr("height", 0)
-        .attr("width", x.bandwidth() - 2)
-        .attr("x", function (d, i) {
-            return x(i);
-        })
-        .attr("fill", "steelblue")
+        .attr("class", "bar")
+        .attr("y",height)
+        .attr("height",0)
+        .attr("width", x.bandwidth())
+        .attr("x", function (d) {return x(d.labels);})
+        .attr("fill","steelblue")
         .transition()
-        .attr("height", function (d) {
-            return height - y(d);
-        })
-        .attr("y", function (d) {
-            return y(d);
-        })
+        .attr("height", function (d) { return height - y(d.Prob); })
+        .attr("y", function (d) {return y(d.Prob);})
         .duration(1000);
 
-    var title = svg.append("text")
-        .style("font-size", "20px")
-        .text('My title')
-        .attr("x", width / 2 + margin.left)
-        .attr("y", 30)
-        .attr("text-anchor", "middle");
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // add the y Axis
+    svg.append("g")
+        .call(yAxis);
 
     // return div.node();
 
