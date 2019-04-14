aeff10eca7b49671b67679a1c60d74532a08e5e4
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2019-04-14 19:24:34 +0100

    almost there...

--- dashboard/js/summary-tab.js
+++ dashboard/js/summary-tab.js
@@ -12,11 +12,15 @@ $('#overview-tab').on('shown.bs.tab', function (e) {
     // hide the toolip raised by the section chart
     d3.select('#tooltip').style('opacity', 0)
 
-    renderSubHeatmapManager()
+    var arg = [];
+    arg.mode = 'constrained';
+    arg.norm = 'mean';
+
+    renderSubHeatmapManager(arg)
 });
 
 
-function renderSubHeatmapManager() {
+function renderSubHeatmapManager(arg) {
 
     d3.csv("./notebooks/cm_summary.csv", function(data){
         data.forEach(function(d) {
@@ -25,29 +29,29 @@ function renderSubHeatmapManager() {
             d.val = +d.val;
         });
 
-        var filter = {mode:'constrained', norm:'mean', fold:0};
+        var filter = {mode:arg.mode, norm:arg.norm, fold:0};
         chartObj.divId = '#summary-tab-chart-1';
         chartObj.clipId = 'clipSubHeatMap-1'; // WITHOUT THE # SYMBOL
         chartObj.clipHashId = '#clipSubHeatMap-1';
-        subplot(data, filter, chartObj)
+        subplot(data, filter, chartObj);
 
-        var filter2 = {mode:'constrained', norm:'mean', fold:1};
+        var filter2 = {mode:arg.mode, norm:arg.norm, fold:1};
         chartObj2.divId = '#summary-tab-chart-2';
         chartObj2.clipId = 'clipSubHeatMap-2'; // WITHOUT THE # SYMBOL
         chartObj2.clipHashId = '#clipSubHeatMap-2';
-        subplot(data, filter2, chartObj2)
+        subplot(data, filter2, chartObj2);
 
-        var filter3 = {mode:'constrained', norm:'mean', fold:2};
+        var filter3 = {mode:arg.mode, norm:arg.norm, fold:2};
         chartObj3.divId = '#summary-tab-chart-3';
         chartObj3.clipId = 'clipSubHeatMap-3'; // WITHOUT THE # SYMBOL
         chartObj3.clipHashId = '#clipSubHeatMap-3';
-        subplot(data, filter3, chartObj3)
+        subplot(data, filter3, chartObj3);
 
-        var filter4 = {mode:'constrained', norm:'mean', fold:3};
+        var filter4 = {mode:arg.mode, norm:arg.norm, fold:3};
         chartObj4.divId = '#summary-tab-chart-4';
         chartObj4.clipId = 'clipSubHeatMap-4'; // WITHOUT THE # SYMBOL
         chartObj4.clipHashId = '#clipSubHeatMap-4';
-        subplot(data, filter4, chartObj4)
+        subplot(data, filter4, chartObj4);
 
 
         // subplot('mean')
