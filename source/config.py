e35409aa7979e3dda1a8afbb3f0aa9beea310ffe
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-06-12 16:49:59 +0100

    Added config file

---
+++ source/config.py
@@ -0,0 +1,13 @@
+DEFAULT = {
+    'CellCallMaxIter': 100,
+    'CellCallTolerance': 0.02,
+    'Inefficiency': 0.2,
+    'InsideCellBonus': 2,
+    'MisreadDensity': 0.00001,
+    'SpotReg': 0.1,
+    'nNeighbors': 3,
+    'rGene': 20,
+    'rSpot': 2,
+    'label_image': '../data/default/CellMap.mat',
+    'roi': {"x0": 6150, "x1": 13751, "y0": 12987, "y1": 18457},
+}
