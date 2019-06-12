aa932e189a999894f3e5df9dd2c0487f9f31ec5a
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-06-12 16:51:48 +0100

    Spots instances need more work, they dont work

---
+++ source/main.py
@@ -0,0 +1,39 @@
+from source.utils import loadmat
+from source.systemData import Cells
+from source.systemData import Spot
+import source.config as config
+import starfish as sf
+import xarray as xr
+import os
+import logging
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+logger = logging.getLogger()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
+
+
+roi = config.DEFAULT['roi']
+label_image_path = config.DEFAULT['label_image']
+saFile = "D:\Dimitris\OneDrive - University College London\dev\Python\spacetx\data_preprocessed\spot_attributes.nc"
+
+label_image = loadmat(os.path.join(label_image_path))
+print("reading CellMap from %s" % label_image_path)
+label_image = label_image["CellMap"]
+
+cells = Cells(label_image, config.DEFAULT)
+c = cells.collection[0]
+
+
+logger.info('********* Getting spotattributes from %s **********', saFile)
+sa = sf.types.SpotAttributes(xr.open_dataset(saFile).to_dataframe())
+
+spots = [Spot(1, d.x, d.y) for d in sa.data]
+#
+# sa.data.target
+# sa.data.x
+# sa.data.y
