139b80964f7501dd25f88c3424f38cf337c262b0
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2019-08-10 23:12:35 +0100

    minor change

--- source/config.py
+++ source/config.py
@@ -11,5 +11,5 @@ DEFAULT = {
     'label_image': '../demo_data/CellMap.mat',
     'roi': {"x0": 6150, "x1": 13751, "y0": 12987, "y1": 18457},
     'geneset': '../demo_data/GeneSet.mat',
-    'saFile': "../demo_data/spots.csv", # Spot attributes, contains spots x,y coordinates and their gene names
+    'saFile': "../demo_data/spots.csv", # Spot attributes, contains x,y coordinates for the spots and their gene names
 }
