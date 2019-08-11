cb8ff56f6039563c558366062fb3f8165d9b9cd8
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2019-08-11 12:02:56 +0100

    minor change

--- testing_vips.py
+++ testing_vips.py
@@ -10,13 +10,19 @@ logging.basicConfig(
     )
 
 dim = 32768
-# roi = {"x0": 6150, "x1": 13751, "y0": 12987, "y1": 18457}
-roi = {"y0": 6150, "y1": 13751, "x0": 12987, "x1": 18457}
+roi = {"x0": 6150, "x1": 13751, "y0": 12987, "y1": 18457}
+# roi = {"y0": 6150, "y1": 13751, "x0": 12987, "x1": 18457}
 
-img_path = 'background_boundaries2.tif'
+img_path = 'background_boundaries.tif'
 out_dir = str(dim) + 'px'
+# remove the dir if it exists
+if os.path.exists(out_dir):
+    shutil.rmtree(out_dir)
 
 im = pyvips.Image.new_from_file(img_path, access='sequential')
+
+# The following two lines add an alpha component to rgb which allows for transparency.
+# Is this worth it? It adds quite a bit on the execution time, about x2 increase
 im = im.colourspace('srgb')
 im = im.addalpha()
 
@@ -32,12 +38,8 @@ im = im.resize(factor)
 # extend with 0 (transparent) pixels
 im = im.gravity('south-west', dim, dim)
 
-logger.info('Started doing the image tiles ')
-if os.path.exists(out_dir):
-    # remove the dir if it exists
-    shutil.rmtree(out_dir)
-
 # now you can create a fresh one and populate it with tiles
+logger.info('Started doing the image tiles ')
 im.dzsave(out_dir, layout='google', suffix='.png', background=0, skip_blanks=0)
 logger.info('Done. Pyramid of tiles saved at: %s' % out_dir)
 print('info')
